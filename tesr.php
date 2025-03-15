420f33f896104142ca1ce39ac3ee9cb4cfddf132
Ratha-121104 <Ratha121104@gmail.com>
2025-03-15 22:21:10 +0700

    update function

--- tesr.php
+++ tesr.php
@@ -1,15 +1,13 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
     $errors = [];
 
-  
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
     $password = htmlspecialchars($_POST['password']);
     $confirm_password = htmlspecialchars($_POST['confirm_password']);
 
-
+  
     if (empty($name) || empty($email) || empty($password) || empty($confirm_password)) {
         $errors['fields'] = "All fields are required.";
     }
@@ -25,7 +23,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "<p><strong>Password:</strong> $password</p>";
       
     } else {
-      
+     
         echo "<h2>Form Submission Failed!</h2>";
         foreach ($errors as $error) {
             echo "<p>$error</p>";
